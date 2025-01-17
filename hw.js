4a89910fa5fd4f0568c5e355e10e090478aeecb9
Dariia Khmara <dariyat98@gmail.com>
2025-01-16 23:30:36 -0500

    Did the homework for the class on the 16th of January 2025

--- hw.js
+++ hw.js
@@ -1,1 +1,17 @@
-<h2>hola!</h2>
+let hours;
+
+do {
+    hours = prompt('Please input amount of hours you would like to convert to seconds');
+    if (hours === null) {
+        alert('Operation cancelled');
+        break;
+    } else if (hours === '' || isNaN(hours)) {
+        alert('Please input a valid number');
+    }
+} while (hours === '' || isNaN(hours));
+
+if (hours !== null && hours !== '' && !isNaN(hours)) {
+    hours = Number(hours); // Convert the input to a number - because it's a string by default
+    let seconds = hours * 3600;
+    alert('In ' + hours + " hour(s) " + 'there are ' + seconds + " seconds");
+}
