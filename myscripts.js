763a970c2f147dbd19edb251866f56e705cb91e8
Dariia K <166187273+clouddari@users.noreply.github.com>
2025-02-11 22:17:29 -0500

    hw10

--- myscripts.js
+++ myscripts.js
@@ -1,105 +1,76 @@
-// 20-30 05
-
-let i = 20;
-let result = "";
-
-do{
-    i += 0.5;
-    result += i + " ";
-
-} while (i < 30);
-
-alert(result);
-
-//Один долар коштує 40 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів
-
-let uah = 0; 
-let result2 = "";
-
-do{
-    uah += 10 ;
-    result2 += `${uah} доларів коштує ${uah * 40} ГРИВЕНЬ \n`;
-
-}while (uah < 100);
+let arr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
 
-alert(result2);
+const positive = arr.filter(a => a > 0);
 
-//Дано ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N
+let sumPositiveInitial = 0; 
+const sumPositive = positive.reduce((accumulator, currentValue) => accumulator + currentValue, sumPositiveInitial,
+ );
 
-let n = prompt("enter ціле число");
-n = Number(n);
+ //Знайти суму та кількість позитивних елементів.
 
-let k = 1; 
-result = " ";
+console.log(`кількість додатних елементів: ${positive.length}`);
+console.log(`cума чисел більше нуля: ${sumPositive}`)
 
 
-while (k * k <= n && k <= 100){
-    result += k + " ";
-    k++
- }
 
- alert(result);
+//Знайти максимальний елемент масиву та його порядковий номер.
+let max = Math.max(...arr);
+let maxIndex = arr.findIndex(a => a === max);
 
+console.log(`найбільше число масиву: ${max},
+його порядковий номер: ${maxIndex + 1 }
+`);
+
+// Знайти мінімальний елемент масиву та його порядковий номер.
+
+let min = Math.min(...arr);
+let minIndex = arr.findIndex(a => a === min);
+
+console.log(`найменше число масиву: ${min},
+його порядковий номер: ${minIndex + 1 }`);
+
+
+//Визначити кількість негативних елементів.
+let negative = arr.filter(a => a < 0);
+console.log(`кількість від'ємних елементів: ${negative.length}`);
+
+//Знайти кількість непарних позитивних елементів.
+let odd = positive.filter(a => a % 2 !== 0);
+console.log(`непарні додатні елементи(кількість): ${odd.length}`)
+
+
+//Визначити кількість парних позитивних елементів.
+let even = positive.filter(a => a % 2 === 0);
+console.log(`парні додатні елементи(кількість): ${even.length}`)
+
+//Знайти суму парних позитивних елементів.
+const sumPositiveEven = even.reduce((accumulator, currentValue) =>
+     accumulator + currentValue, sumPositiveInitial,
+ );
  
+ console.log(`сума парних додатніх елементів: ${sumPositiveEven}`)
 
- //Дано ціле число. 
- // З'ясувати, чи воно є простим 
- // (простим називається число, більше ніж 1, які мають інших дільників крім 1 і себе).
+//Знайти суму непарних позитивних елементів.
+const sumPositiveOdd = odd.reduce((accumulator, currentValue) =>
+    accumulator + currentValue, sumPositiveInitial,
+);
 
- let numNum = prompt("введіть ціле число більше 1 щоб перевірити чи воно є простим");
- numNum = Number(numNum);
-
- let dil = 2; 
- let isPrime = true;
-
- while(dil <=  Math.sqrt(numNum)){
-        if (numNum % dil === 0){
-            isPrime = false;
-            break;
-        }
-        dil++;
- }
-
- if (isPrime && numNum > 1) {
-    alert("число просте!")
- } else {
-    alert("число не просте")
- }
+console.log(`сума непарних додатніх елементів: ${sumPositiveOdd}`);
 
 
-// Дано деяке число. 
-// Ваше завдання – визначити, чи можна отримати це число,
-//  піднявши число 3 до певного натурального ступеня.
-//  (Як приклад, числа 9 та 81 можна отримати цим способом, але 13 – ні.)
+//Знайти добуток позитивних елементів.
+let productInitial = 1;
+const product = positive.reduce((accumulator, currentValue) =>
+    accumulator * currentValue, productInitial,
+);
 
-let numberito = prompt("введіть число, щоб перевірити чи можна його отримати піднявши інше число до 3 натурального степеня");
-numberito = Number(numberito);
-
-let found = false; 
-
-for (let h = 1; h <= Math.cbrt(numberito); h++){
-    if (h * h * h === numberito){
-        found = true;
-        break;
-    }
-}
-
-for (let h = 1; h <= Math.sqrt(numberito); h++){
-    if (h * h  === numberito){
-        found = true;
-        break;
-    }
-}
+console.log(`добуток додатніх елементів: ${product}`);
 
 
-if (found) {
-    alert("Так, це число можна отримати, піднявши інше число до квадрата або куба.");
-} else {
-    alert("Ні, таке число не можна отримати.");
-}
+//Знайти найбільший серед елементів масиву, решту занулити.
 
-
-
+const maxNull = arr.map(el => (el === max  ? el : 0));
+console.log(maxNull);
 
 
