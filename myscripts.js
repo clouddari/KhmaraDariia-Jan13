aad0a1a673c0f6b4a7f116521e80e9ae9b6940cc
Dariia K <166187273+clouddari@users.noreply.github.com>
2025-02-24 11:06:57 -0500

    added a confition for cases where an element doesn't exist

--- myscripts.js
+++ myscripts.js
@@ -2,11 +2,16 @@
 //  щоб видалити елемент item з масиву array.
 
 let array = [1, 2, 3, 4, 5, 6, 7];
-console.log(array);
+console.log(`початковий масив ${array}`);
 
 function removeElement(array, item) {
-    let result = array.splice(array.indexOf(item), 1);
+    let index = array.indexOf(item);
+    if (index !== -1) {
+        array.splice(index, 1);
+    } else {
+        console.error(`Error: Item ${item} not found in the array.`);
+    }
     return array;
 }
 
-console.log(removeElement(array, 5));
+console.log(`масив з видаленим елементом (якщо він існує) - ${removeElement(array, 4)}`);
