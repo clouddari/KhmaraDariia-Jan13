98c1facdd5917ca558b8e05444faa04baa20a742
Dariia K <166187273+clouddari@users.noreply.github.com>
2025-03-15 18:59:40 -0400

    hw18 this chain

--- myscripts.js
+++ myscripts.js
@@ -1,39 +1,18 @@
-//робимо функцію яка генеруватиме рандом номери телефону  -- generateRandomPhoneNumbers
-//user вирішує скільки номерів генерувати
-
-const lastTenNumbers = []; //масив для збереження останніх 10 номерів;
-
-function generateRandomPhoneNumbers(amount) {
-
-  function generatePhoneNumber(){
-    let phoneNumber = "+380";
-
-    for (let i = 0; i < 9; i++) {
-      let digit = Math.floor(Math.random() * 10);
-      phoneNumber += digit;
-    }
-
-    lastTenNumbers.push(phoneNumber);//додаємо номер  у кеш
-
-    if (lastTenNumbers.length > 10){
-      lastTenNumbers.shift()
-    }
-
-    return phoneNumber;
+let ladder = {
+  step: 0,
+  up: function () {
+      this.step++;
+      return this;
+  },
+  down: function () {
+      this.step--;
+      return this;
+  },
+  showStep: function () { // показує теперішню сходинку
+      alert(this.step);
   }
-  
-  let phoneNumbers = [];
+};
 
-  for (let i = 0; i < amount; i++) {
-      let phoneNumber = generatePhoneNumber(); 
-      phoneNumbers.push(phoneNumber);
-  }
-  
-  return phoneNumbers;
-}
+ladder.up().up().down().showStep(); // 1
 
-console.log(generateRandomPhoneNumbers(7));
-console.log(generateRandomPhoneNumbers(8));
-console.log("Останні 10 номерів:", lastTenNumbers);
 
-// lastTenNumbers.length = 0; //якщо треба очистити кеш
