083cdf73519b3649c45c75cce18bc191ab08cee4
Dariia K <166187273+clouddari@users.noreply.github.com>
2025-03-20 19:27:17 -0400

    hw21

--- myscripts.js
+++ myscripts.js
@@ -1,14 +1,56 @@
-const items = document.querySelectorAll(".item");
-const counters = document.querySelectorAll(".counter");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+let imageDiv = document.querySelector(".imageDiv");
 
-let countersArray = new Array(items.length).fill(0);
+const images = [
+  "img1.jpg",
+  "img2.jpg",
+  "img3.jpg",
+  "img4.avif",
+  "img5.jpeg",
+  "img6.jpg",
+  "img7.jpg",
+  "img8.jpg",
+  "img9.webp",
+  "img10.jpeg",
+];
 
-items.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    countersArray[index]++; 
-    counters[index].textContent = countersArray[index];
+let image = document.createElement("img");
+imageDiv.appendChild(image);
+let currentIndex = 0;
+
+const updateImage = () => {
+  image.src = images[currentIndex];
+  image.width = 250;
+  image.height = 250;
+
+  if(currentIndex === images.length - 1){
+    nextButton.style.display = "none";
+  } else {
+    nextButton.style.display = "inline";
+  }
+  
+  if(currentIndex === 0){
+   prevButton.style.display = "none";
+  } else {
+    prevButton.style.display = "inline";
+  }
+  
+}
+
+prevButton.addEventListener("click", () => {
+    if(currentIndex > 0){
+      currentIndex--;
+      updateImage();
+    }
   })
+
+nextButton.addEventListener("click", () => {
+  if(currentIndex < images.length - 1){
+    currentIndex++;
+    updateImage();
+  }
 })
 
 
-
+updateImage();
