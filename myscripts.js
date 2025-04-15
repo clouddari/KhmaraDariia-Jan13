337b24a56a6d5f3b6bf3eb9ce20aacd4311a9a78
Dariia K <166187273+clouddari@users.noreply.github.com>
2025-04-15 00:04:47 -0400

    hw34

--- myscripts.js
+++ myscripts.js
@@ -1,27 +1,24 @@
 const imgPath = (digit) => `/number_${digit}.webp`;
 
-const updateDigit = (id, digit) => {
-  const img = document.getElementById(id);
-  const newSrc = imgPath(digit);
+const previousButton = document.querySelector("#prev");
+const nextButton = document.querySelector("#next");
+const image = document.querySelector("#h1");
 
-  if (!img.src !== newSrc) {
-    img.src = newSrc;
-  }
+let currentDigit = 0;
+
+previousButton.addEventListener("click", () => {
+  currentDigit = (currentDigit - 1 + 10) % 10;
+  image.src = imgPath(currentDigit);
+});
+
+nextButton.addEventListener("click", () => {
+  currentDigit = (currentDigit + 1) % 10;
+  image.src = imgPath(currentDigit);
+});
+
+const updateImage = () => {
+  image.src = imgPath(currentDigit);
+  currentDigit = (currentDigit + 1) % 10;
 };
 
-const updateClock = () => {
-  const now = new Date();
-  const h = now.getHours().toString().padStart(2, "0");
-  const m = now.getMinutes().toString().padStart(2, "0");
-  const s = now.getSeconds().toString().padStart(2, "0");
-
-  updateDigit("h1", h[0]);
-  updateDigit("h2", h[1]);
-  updateDigit("m1", m[0]);
-  updateDigit("m2", m[1]);
-  updateDigit("s1", s[0]);
-  updateDigit("s2", s[1]);
-};
-
-setInterval(updateClock, 1000);
-updateClock();
+setInterval(updateImage, 3000);
