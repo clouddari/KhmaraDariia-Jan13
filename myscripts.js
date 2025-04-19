fdfbbf5fcc8fb48918fbb3e6c7c6b5f94c7233ec
Dariia K <166187273+clouddari@users.noreply.github.com>
2025-04-18 20:48:26 -0400

    hw35

--- myscripts.js
+++ myscripts.js
@@ -1,24 +1,69 @@
-const imgPath = (digit) => `/number_${digit}.webp`;
+const responses = [
+  "😍 круто! ",
+  "no way 🫨",
+  "ЩооОООо? ⁉️",
+  "дякую 👌",
+  "обожнюю цю погоду 🌡️☁️🌞",
+  "час придбати велосипед 🚲",
+  "🤒i`m sick of that",
+  "🐾🐾🐾🐾🐾🐾🐾meow🐈‍⬛🐈‍⬛",
+  "🕷️boo🕸️",
+];
 
-const previousButton = document.querySelector("#prev");
-const nextButton = document.querySelector("#next");
-const image = document.querySelector("#h1");
+const sendButton = document.querySelector("button");
+const inputField = document.querySelector("input");
 
-let currentDigit = 0;
-
-previousButton.addEventListener("click", () => {
-  currentDigit = (currentDigit - 1 + 10) % 10;
-  image.src = imgPath(currentDigit);
+inputField.addEventListener("input", () => {
+  if (
+    inputField.value.trim() !== "" &&
+    inputField.value !== "почніть писати..."
+  ) {
+    sendButton.disabled = false;
+  } else {
+    sendButton.disabled = true;
+  }
 });
 
-nextButton.addEventListener("click", () => {
-  currentDigit = (currentDigit + 1) % 10;
-  image.src = imgPath(currentDigit);
-});
+async function handleMessage() {
+  const input = inputField.value.trim();
+  const responsesDiv = document.querySelector(".chat-messages");
 
-const updateImage = () => {
-  image.src = imgPath(currentDigit);
-  currentDigit = (currentDigit + 1) % 10;
-};
+  function addMessage(text) {
+    const p = document.createElement("p");
+    p.textContent = text;
+    responsesDiv.appendChild(p);
+    return p;
+  }
 
-setInterval(updateImage, 3000);
+  function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  if (input === "My watch has ended") {
+    addMessage("чат завершено");
+    sendButton.disabled = true;
+    return;
+  } else if (!input) {
+    addMessage("введіть повідомлення щоб отримати відповідь");
+    sendButton.disabled = true;
+  } else {
+    addMessage(input);
+    inputField.value = "";
+    const thinking = addMessage("чат-бот набирає повідомлення...");
+
+    await wait(Math.random() * 9000 + 1000);
+    responsesDiv.removeChild(thinking);
+
+    const chance = Math.random();
+    if (chance < 0.1) {
+      addMessage("мені раптом набридло...💤💤💤 чат завершено");
+      sendButton.disabled = true;
+      return;
+    }
+
+    let randomMessage = responses[Math.floor(Math.random() * responses.length)];
+    addMessage(randomMessage);
+  }
+}
+
+sendButton.addEventListener("click", handleMessage);
